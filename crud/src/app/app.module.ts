95bbde6bca743871c455ebce85a0ee25032b9219
karen <karen@outlab.rio>
2021-03-21 19:53:18 -0300

    Imports para map API

--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApiServiceService } from './services/api-service.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ApiServiceService,
+  HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
